22f6654da89e537818d632a14388e1365366beaa
Daffa <daffaaqdas@gmail.com>
2026-04-07 16:41:06 +0700

    Intergrated Hand Rank into Scoring Rule

--- ScoringRule.cpp
+++ ScoringRule.cpp
@@ -13,14 +13,32 @@ return score;
 }
 int ScoringRule::convertRankToScore(HandRank rank){
 switch (rank){
-case HandRank::FULL_HOUSE:
-return 25;
-case HandRank::FLUSH:
-return 20;
-case HandRank::PAIR:
-return 10;
-case HandRank::HIGH_CARD:
-default:
-return 5;
+    case HandRank::FLUSH_FIVE:
+    return 65;
+    case HandRank::FLUSH_HOUSE:
+    return 60;
+    case HandRank::FIVE_OF_A_KIND:
+    return 55;
+    case HandRank::ROYAL_FLUSH:
+    return 50;
+    case HandRank::STRAIGHT_FLUSH:
+    return 45;
+    case HandRank::FOUR_OF_A_KIND:
+    return 40;
+    case HandRank::FULL_HOUSE:
+    return 35;
+    case HandRank::FLUSH:
+    return 30;
+    case HandRank::STRAIGHT:
+    return 25;
+    case HandRank::THREE_OF_A_KIND:
+    return 20;
+    case HandRank::TWO_PAIR:
+    return 15;
+    case HandRank::PAIR:
+    return 10;
+    case HandRank::HIGH_CARD:
+    default:
+    return 5;
 }
 } 
--- ScoringRule.h
+++ ScoringRule.h
@@ -1,16 +1,38 @@
 #pragma once
 #include "Hand.h"
 #include "PokerHandChecker.h"
-#include "FullHouseChecker.h"
-#include "FlushChecker.h"
+#include "HighCardChecker.h"
 #include "PairChecker.h"
+#include "TwoPairChecker.h"
+#include "ThreeOfAKindChecker.h"
+#include "StraightChecker.h"
+#include "FlushChecker.h"
+#include "FullHouseChecker.h"
+#include "FourOfAKindChecker.h"
+#include "StraightFlushChecker.h"
+#include "RoyalFlushChecker.h"
+#include "FiveOfAKindChecker.h"
+#include "FlushHouseChecker.h"
+#include "FlushFiveChecker.h"
+
 class ScoringRule{
 public:
 ScoringRule();
 int scoreHand(const Hand& hand);
 private:
-FullHouseChecker fullHouseChecker;
-FlushChecker flushChecker;
+HighCardChecker highCardChecker;
 PairChecker pairChecker;
+TwoPairChecker twoPairChecker;
+ThreeOfAKindChecker threeOfAKindChecker;
+StraightChecker straightChecker;
+FlushChecker flushChecker;
+FullHouseChecker fullHouseChecker;
+FourOfAKindChecker fourOfAKindChecker;
+StraightFlushChecker straightFlushChecker;
+RoyalFlushChecker royalFlushChecker;
+FiveOfAKindChecker fiveOfAKindChecker;
+FlushHouseChecker flushFiveChecker;
+FlushFiveChecker flushFiveChecker;
+
 int convertRankToScore(HandRank rank);
 };
