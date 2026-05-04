ce0170c79ac81137e14fb7b2efce9012ff02766f
Daffa <daffaaqdas@gmail.com>
2026-05-04 15:49:12 +0700

    Add Card.h, Rank.h, and, Suit.h

---
+++ Rank.h
@@ -0,0 +1,15 @@
+#pragma once
+
+const int RANK_2 = 2;
+const int RANK_3 = 3;
+const int RANK_4 = 4;
+const int RANK_5 = 5;
+const int RANK_6 = 6;
+const int RANK_7 = 7;
+const int RANK_8 = 8;
+const int RANK_9 = 9;
+const int RANK_10 = 10;
+const int RANK_JACK = 11;
+const int RANK_QUEEN = 12;
+const int RANK_KING = 13;
+const int RANK_ACE = 14;
---
+++ Suit.h
@@ -0,0 +1,6 @@
+#pragma once
+
+const char SUIT_SPADES   = 'S'; 
+const char SUIT_HEARTS   = 'H'; 
+const char SUIT_CLUBS    = 'C'; 
+const char SUIT_DIAMONDS = 'D'; 
