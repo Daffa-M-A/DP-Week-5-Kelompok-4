1349809ba3c211b574c469035dacadbf7937ae9e
Daffa <daffaaqdas@gmail.com>
2026-06-01 09:56:21 +0700

    Apply joker into game manager

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include "GameManager.h"
+#include "ConcreteJoker.h" 
 
-void GameManager::runSession(){
+GameManager::GameManager() 
+    : shopSystem(jokerManager, handTable, gameDeck), playerWallet(10) {}
+
+void GameManager::runSession() {
     std::cout << "=== Run Started ===\n";
-    
+
     Hand hand = handGenerator.generateRandomHand(8);
-    
     handPlayer.playHand(hand);
-    int score = scoringRule.scoreHand(hand);
+    Hand chosenHand = handPlayer.getChosenHand();
+    int score = scoringRule.scoreHand(chosenHand, jokerManager);
     bool win = blindRule.checkBlind(score);
     int reward = rewardRule.earnMoney(win, score);
+    std::cout << "Money gained from round: $" << reward << "\n";
     
-    std::cout << "Money gained: " << reward << "\n";
+    playerWallet.add(reward);
+    
+    std::cout << "\nSHOP\n";
+    
+    std::vector<std::string> itemToStock = {"JOKER_ADDMULT", "JOKER_PAIR", "JOKER_DIAMOND"};
+    shopSystem.generateShopFront(itemToStock);
+    
+    shopSystem.displayShop(playerWallet);
+    
+    std::cout << "[Player Action] Mencoba membeli item indeks 0...\n";
+    shopSystem.buyItem(0, playerWallet);
+    
+    std::cout << "========================================\n\n";
     std::cout << "=== Run Ended ===\n";
 }
--- GameManager.h
+++ GameManager.h
@@ -4,13 +4,25 @@
 #include "ScoringRule.h"
 #include "BlindRule.h"
 #include "RewardRule.h"
-class GameManager{
+#include "JokerManager.h"
+#include "ShopSystem.h"
+#include "ShopData.h" 
+#include "GameSystemsMock.h" 
+
+class GameManager {
 public:
-void runSession();
+    GameManager();
+    void runSession();
+
 private:
-HandGenerator handGenerator;
-HandPlayer handPlayer;
-ScoringRule scoringRule;
-BlindRule blindRule;
-RewardRule rewardRule;
+    HandGenerator handGenerator;
+    HandPlayer handPlayer;
+    ScoringRule scoringRule;
+    BlindRule blindRule;
+    RewardRule rewardRule;
+    JokerManager jokerManager;
+    HandScoreTable handTable;
+    Deck gameDeck;
+    ShopSystem shopSystem;
+    Money playerWallet; 
 };
